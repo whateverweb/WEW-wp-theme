74b7223aa968b0fc61e77231dd2bd0fb0eadbe9e
Florin <ft@mobiletech.no>
2013-05-14 14:21:47 +0300

    changed footer template

--- docs-footer.php
+++ docs-footer.php
@@ -5,28 +5,21 @@
 ?>
 		</div><!-- #page -->
 		</div><!-- .page-wrapper -->
-		<footer class="docs-page">
-        	
-        		<div id="footer-social">
-                	<a class="footer-github" href="https://github.com/whateverweb" target="_blank">GitHub</a>
-                    <a class="footer-twitter" href="https://twitter.com/WhatEverWeb" target="_blank">Twitter</a>
-					<a class="footer-blog" href="http://blog.ftudoran.com/">Blog</a>
-                </div>
-                <h3>WhateverWeb <span>Let&rsquo;s bring web into the future</span></h3>									
-                <p>Copyright &copy; 2013-2014 WhateverWeb.com<br />Brought to you by Mobiletech</p>                
-          
+		<footer class="docs-page">        	
+            <div id="footer-social">
+                <a class="footer-github" href="https://github.com/whateverweb" target="_blank">GitHub</a>
+                <a class="footer-twitter" href="https://twitter.com/WhatEverWeb" target="_blank">Twitter</a>
+                <a class="footer-blog" href="http://docs.whateverweb.com/">Blog</a>
+            </div>
+            <h3>WhateverWeb <span>Let&rsquo;s bring web into the future</span></h3>									
+            <p>Copyright &copy; 2013-2014 WhateverWeb.com<br />Brought to you by Mobiletech</p>
         </footer>
         
-        <!-- JS 
-		<script src="scripts/whatever.config.js"></script>
-		<script src="scripts/whatever.utils.js"></script>
-		<script src="widgets/js/ajax.js"></script>
-		<script src="widgets/js/modalview.js"></script>
-		<script src="scripts/guardian.js"></script>
-		<script src="widgets/js/animator.js"></script>
-		<script src="scripts/animateMenu.js"></script>
-		
-		
+        <script src="http://whateverweb.com/scripts/whatever.config.js"></script>
+		<script src="http://whateverweb.com/widgets/js/ajax.js"></script>
+		<script src="http://whateverweb.com/widgets/js/modalview.js"></script>
+		<script src="http://whateverweb.com/scripts/guardian.js"></script>
+		<script src="http://whateverweb.com/scripts/animateMenu.js"></script>	
 		
 		<script>
 		  var _gaq = _gaq || [];
@@ -39,8 +32,6 @@
 		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 		  })();		
 		</script>
-
-		-->
 		
 		<?php wp_footer(); ?>
     </body>    
--- footer.php
+++ footer.php
@@ -6,34 +6,25 @@
 		<div class="clear-footer"></div>
 		</div><!-- #page -->
 		</div><!-- .page-wrapper -->
-		<footer>
-        	<div class="master-width">
+		<footer>            
+            <div class="master-width">
         		<div id="footer-social">
                 	<a class="footer-github" href="https://github.com/whateverweb" target="_blank">GitHub</a>
                     <a class="footer-twitter" href="https://twitter.com/WhatEverWeb" target="_blank">Twitter</a>
-					<a class="footer-blog" href="http://blog.ftudoran.com/">Blog</a>
                 </div>
-                <h3>WhateverWeb <span>Let&rsquo;s bring web into the future</span></h3> 
-				<?php $menuParameters = array('menu' => 'Footer Main Menu', 'container' => false, 'echo' => false, 'items_wrap' => '%3$s', 'depth' => 0); ?>
-				<p class="footer-menu"><?php echo strip_tags(wp_nav_menu( $menuParameters ), '<a>' ); ?></p>
-				
-				<?php $menuParameters = array('menu' => 'Footer Links', 'container' => false, 'echo' => false, 'items_wrap' => '%3$s', 'depth' => 0); ?>
-				<p class="footer-menu"><?php echo strip_tags(wp_nav_menu( $menuParameters ), '<a>' ); ?></p>							
-                <p>Copyright &copy; 2013-2014 WhateverWeb.com<br />Brought to you by Mobiletech</p>                
-            </div>
+                <h3>WhateverWeb <span>Let’s bring web into the future</span></h3>
+                <p class="footer-menu"><a href="http://whateverweb.com/about.html" class="first">About</a><a href="http://whateverweb.com/getting_started.html">Getting Started</a><a href="http://whateverweb.com/documentation.html">Documentation</a><a class="last" href="mailto:team@whateverweb.com">Support</a><!-- <a class="last" href="blog.html">Blog</a> --></p>
+                <p class="footer-menu"><a href="http://whateverweb.com/terms.html" class="first">Terms of Service</a><a href="http://whateverweb.com/privacy.html" class="last">Privacy Policy</a>
+                </p><p>Copyright © 2013-2014 WhateverWeb.com<br>Brought to you by Mobiletech</p>                
+            </div>            
         </footer>
-        
-        <!-- JS 
-		<script src="scripts/whatever.config.js"></script>
-		<script src="scripts/whatever.utils.js"></script>
-		<script src="widgets/js/ajax.js"></script>
-		<script src="widgets/js/modalview.js"></script>
-		<script src="scripts/guardian.js"></script>
-		<script src="widgets/js/animator.js"></script>
-		<script src="scripts/animateMenu.js"></script>
-		
-		
-		
+       
+		<script src="http://whateverweb.com/scripts/whatever.config.js"></script>
+		<script src="http://whateverweb.com/widgets/js/ajax.js"></script>
+		<script src="http://whateverweb.com/widgets/js/modalview.js"></script>
+		<script src="http://whateverweb.com/scripts/guardian.js"></script>
+		<script src="http://whateverweb.com/scripts/animateMenu.js"></script>
+	
 		<script>
 		  var _gaq = _gaq || [];
 		  _gaq.push(['_setAccount', 'UA-36395921-1']);
@@ -45,9 +36,7 @@
 		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 		  })();		
 		</script>
-
-		-->
-		
+        
 		<?php wp_footer(); ?>
     </body>    
 </html>
