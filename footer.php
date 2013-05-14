77501f98fe787439087dd3b4b831f31639ae5398
Florin <ft@mobiletech.no>
2013-05-14 15:24:56 +0300

    changed headers

--- footer.php
+++ footer.php
@@ -12,10 +12,10 @@
                 	<a class="footer-github" href="https://github.com/whateverweb" target="_blank">GitHub</a>
                     <a class="footer-twitter" href="https://twitter.com/WhatEverWeb" target="_blank">Twitter</a>
                 </div>
-                <h3>WhateverWeb <span>Let’s bring web into the future</span></h3>
+                <h3>WhateverWeb <span>Let&rsquo;s bring web into the future</span></h3>
                 <p class="footer-menu"><a href="http://whateverweb.com/about.html" class="first">About</a><a href="http://whateverweb.com/getting_started.html">Getting Started</a><a href="http://whateverweb.com/documentation.html">Documentation</a><a class="last" href="mailto:team@whateverweb.com">Support</a><!-- <a class="last" href="blog.html">Blog</a> --></p>
                 <p class="footer-menu"><a href="http://whateverweb.com/terms.html" class="first">Terms of Service</a><a href="http://whateverweb.com/privacy.html" class="last">Privacy Policy</a>
-                </p><p>Copyright © 2013-2014 WhateverWeb.com<br>Brought to you by Mobiletech</p>                
+                </p><p>Copyright &copy; 2013-2014 WhateverWeb.com<br>Brought to you by Mobiletech</p>                
             </div>            
         </footer>
        
