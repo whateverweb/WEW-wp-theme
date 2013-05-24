3b8f3c9b0763c4cebeb9a8db12f0253c8531cf58
Florin <ft@mobiletech.no>
2013-05-24 17:15:26 +0300

    added custom excerpt

--- header.php
+++ header.php
@@ -5,16 +5,7 @@
 ?>
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
-    <head>   
-		<!-- FavIcon -->
-		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">	
-		<meta name="msapplication-TileColor" content="#e34947">
-		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/images/favicon64x64w.png">
-       	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png" />
-		<link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/images/favicon57x57">		
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/images/favicon72x72.png" />
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/images/favicon114x114.png" />
-		
+    <head> 
     	<!-- META -->
         <meta charset="UTF-8" />
         <meta name="author" content="Mobiletech AS" />
@@ -22,7 +13,16 @@
         <title><?php wp_title( '|', true, 'right' ); ?></title>
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-		        
+		
+        <!-- FavIcon -->
+		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">	
+		<meta name="msapplication-TileColor" content="#e34947">
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/images/favicon64x64w.png">
+       	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png" />
+		<link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/images/favicon57x57">		
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/images/favicon72x72.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/images/favicon114x114.png" />
+                
         <!-- CSS -->
         <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic|Sansita+One" />
 		
@@ -41,7 +41,7 @@
 		                            <li class="first"><a href="http://whateverweb.com/myApplications.html">Dashboard</a></li> 
 		                            <li><a href="http://whateverweb.com/securitySettings.html">My Account</a></li>    
 		                            <li><a href="http://whateverweb.com/statisticsQuotaAccount.html">Account Statistics</a></li>    
-		                            <li class="last"><a onclick="myGuardian.logout(); return false;">Logout</a></li>                
+		                            <li class="last"><a onClick="myGuardian.logout(); return false;">Logout</a></li>                
 		                        </ul>
 		                    </nav>
 		                </div>
