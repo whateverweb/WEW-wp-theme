931e841aeedc2cfa8f60655dc50156ad3dadd6cb
Florin <ft@mobiletech.no>
2013-06-25 17:48:13 +0300

    style updates

--- 404.php
+++ 404.php
@@ -8,7 +8,7 @@ get_header(); ?>
 <div class="master-width vspace clearfix">
 	<div class="left-side"> 
 		<h1>404 ?!</h1>
-		<h2>Ups, it seems you have reached the end of the universe. <br />You might want to turn around your spaceship and try some top menu destinations.</h2>
+		<h2>Ups, it seems you have lost <em>data connectivity</em>. <br />You might want to turn around or try some top menu destinations.</h2>
 		<p>
 			<a href="http://www.whateverweb.com/" class="cyan-page-button">Send me Home</a>
 		</p>
