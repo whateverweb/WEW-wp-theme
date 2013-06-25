af982890f5422299e8629e68c573e7286b35ab8d
Florin <ft@mobiletech.no>
2013-06-25 16:55:02 +0300

    style updates

--- content-page.php
+++ content-page.php
@@ -4,6 +4,6 @@
  */
  
 if (have_posts()): while (have_posts()): the_post(); ?>
-	<h2><?php the_title(); ?></h2>
-	<br /><?php the_content(); ?>
+	<h1><?php the_title(); ?></h1>
+	<?php the_content(); ?>
 <?php endwhile; endif; ?>
