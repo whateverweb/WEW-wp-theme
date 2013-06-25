38f66968ee1ffc486dfaefd387d654eaac92d279
Florin <ft@mobiletech.no>
2013-06-25 15:57:28 +0300

    style updates

--- search.php
+++ search.php
@@ -12,12 +12,12 @@ get_template_part('docs-header'); ?>
     <?php if ( have_posts() && trim(get_search_query()) != '' && trim(get_search_query()) != null ) : ?>
     	
         <div class="right-side-columns clearfix">
-        	<h1 class="page-title"><?php echo 'Results for: <span>' . get_search_query() . '</span>'; ?></h1>
+        	<h2 class="search-page-title"><?php echo 'Results for: <span>' . get_search_query() . '</span>'; ?></h2>
         </div>
         
         <?php while ( have_posts() ) : the_post(); ?>
-            <?php //the_title(); ?>
             <div class="right-side-columns clearfix">
+				<h1><a href="<?php the_permalink(); ?>" title="Read <?php the_title(); ?>"><?php the_title(); ?></a></h1>
 				<?php the_excerpt(); ?>
             </div>
         <?php endwhile; ?>
