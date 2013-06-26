3200c02a20633a003bb9d7c0c9c1400b8d358416
Florin <ft@mobiletech.no>
2013-06-26 13:54:36 +0300

    style updates

--- footer.php
+++ footer.php
@@ -11,10 +11,11 @@
         		<div id="footer-social">
                 	<a class="footer-github" href="https://github.com/whateverweb" target="_blank">GitHub</a>
                     <a class="footer-twitter" href="https://twitter.com/WhatEverWeb" target="_blank">Twitter</a>
+                    <a class="footer-blog" href="http://docs.whateverweb.com/introduction/" target="_blank">Docs</a>
                 </div>
                 <h3>WhateverWeb <span>Let&rsquo;s bring web into the future</span></h3>
-                <p class="footer-menu"><a href="http://whateverweb.com/about.html" class="first">About</a><a href="http://whateverweb.com/getting_started.html">Getting Started</a><a href="http://whateverweb.com/documentation.html">Documentation</a><a class="last" href="mailto:team@whateverweb.com">Support</a><!-- <a class="last" href="blog.html">Blog</a> --></p>
-                <p class="footer-menu"><a href="http://whateverweb.com/terms.html" class="first">Terms of Service</a><a href="http://whateverweb.com/privacy.html" class="last">Privacy Policy</a>
+                <p class="footer-menu"><a href="http://whateverweb.com/about.html" class="first">About</a><a href="http://whateverweb.com/getting_started.html">Getting Started</a><a href="http://docs.whateverweb.com/">Blog</a><a href="http://docs.whateverweb.com/introduction/" class="last">Documentation</a></p>
+                <p class="footer-menu"><a href="mailto:team@whateverweb.com" class="first">Support</a><a href="http://whateverweb.com/terms.html">Terms of Service</a><a href="http://whateverweb.com/privacy.html" class="last">Privacy Policy</a>
                 </p><p>Copyright &copy; 2013-2014 WhateverWeb.com<br>Brought to you by Mobiletech</p>                
             </div>            
         </footer>
