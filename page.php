27618870ac42bd125adee5810cf032732ec04cb3
Florin <ft@mobiletech.no>
2013-06-26 13:15:57 +0300

    style updates

--- page.php
+++ page.php
@@ -5,7 +5,6 @@
 
 get_header(); ?>
 	<div class="master-width vspace clearfix">
-    	<div class="blog-header"></div>
 		<?php get_template_part( 'content', 'page' ); ?>
 	</div>
 <?php get_footer(); ?>
